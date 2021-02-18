383ff8b6f12f00badcd294f70bdb5f863c3bc48e
Ishan <ishan19308@iiitd.ac.in>
2021-02-19 04:05:41 +0530

    level1 added

--- bird.js
+++ bird.js
@@ -3,6 +3,9 @@ class Bird {
         this.body = Matter.Bodies.circle(x, y, r);
         Matter.World.add(world, this.body);
         this.r = r;
+        this.gravity = .4;
+        this.vx = 25;
+        this.vy = -10;
     }
 
     show() {
@@ -14,5 +17,12 @@ class Bird {
         imageMode(CENTER);
         image(birdImg, 0, 0, this.r * 2, this.r * 2);
         pop();
+    }
+
+    randomMovement() {
+        const pos = this.body.position;
+        pos.x += this.vx;
+        this.vy+=this.gravity;
+        pos.y += this.vy;
     }
 }
--- sketch.js
+++ sketch.js
@@ -14,8 +14,15 @@ let slingImg1;
 let slingImg2;
 let mode;
 let backImgHome;
+let backImgLevel;
 let playImg;
 let Second;
+let l1Img;
+let l2Img;
+let l3Img;
+let backImg;
+let birds = [];
+
 
 function preload() {
   birdImg = loadImage('images/bird.png');
@@ -27,6 +34,11 @@ function preload() {
   slingImg2 = loadImage('images/sling2.png');
   backImgHome = loadImage('images/homebg.jpg');
   playImg = loadImage('images/playButton.png')
+  backImgLevel = loadImage('images/backLevel.png')
+  l1Img = loadImage('images/level1.png');
+  l2Img = loadImage('images/level2.png');
+  l3Img = loadImage('images/level3.png');
+  backImg = loadImage('images/back.png');
 }
 
 function setup() {
@@ -39,10 +51,7 @@ function setup() {
   // for (let i=0;i<3;i++){
   //   boxes[i] = new ColumnBox(450, 300-i*75, 50, 75);
   // }
-  level1();
-
-  bird = new Bird(250, 575, 25);
-  slingshot = new Slingshot(250, 575, bird.body);
+  // level1();
 
   const canvasMouse = Mouse.create(canvas.elt);
   canvasMouse.pixelRatio = pixelDensity();
@@ -57,6 +66,15 @@ function setup() {
 }
 
 function level1() {
+  if (bird != undefined) {
+    World.remove(world, bird.body);
+  }
+  bird = new Bird(250, 575, 25);
+  for (let i = 0; i < boxes.length; i++) {
+    World.remove(world, boxes[i].body);
+  }
+  // World.remove(world, bird.body);
+  slingshot = new Slingshot(250, 575, bird.body);
   boxes[0] = new ColumnBox(1000, 500, 19, 82);
   boxes[1] = new ColumnBox(1083, 500, 19, 82);
   boxes[2] = new RowBox(1041, 450, 83, 21);
@@ -72,7 +90,7 @@ function level1() {
 }
 
 function keyPressed() {
-  if (mode == 1) {
+  if (mode == 2) {
     if (key = ' ') {
       World.remove(world, bird.body);
       bird = new Bird(250, 575, 25);
@@ -83,7 +101,7 @@ function keyPressed() {
 
 
 function mouseReleased() {
-  if (mode == 1 && Math.abs((second() - Second))>2) {
+  if (mode == 2 && Math.abs((second() - Second)) > 1) {
     setTimeout(() => {
       slingshot.fly();
     }, 80)
@@ -93,8 +111,33 @@ function mouseReleased() {
 function mousePressed() {
   if (mode == 0) {
     if (mouseX > 650 && mouseY > 375 && mouseX < 853 && mouseY < 475) {
-      Second = second();
+      Second = second(); //new game debug error
       mode = 1;
+    }
+  }
+  if (mode == 1) {
+    if (dist(mouseX, mouseY, 70, 100) < 50) {
+      mode = 0;
+    }
+    if (mouseX > 150 && mouseY > 300 && mouseX < 377 && mouseY < 527 && Math.abs((second() - Second)) > 1) {
+      mode = 2;
+      level1();
+      Second = second(); //new game debug error
+    }
+    if (mouseX > 650 && mouseY > 300 && mouseX < 877 && mouseY < 527 && Math.abs((second() - Second)) > 1) {
+      mode = 2;
+      Second = second(); //new game debug error
+    }
+    if (mouseX > 1150 && mouseY > 300 && mouseX < 1377 && mouseY < 527 && Math.abs((second() - Second)) > 1) {
+      mode = 2;
+      Second = second(); //new game debug error
+    }
+  }
+  
+  if (mode == 2) {
+    if (dist(mouseX, mouseY, 70, 100) < 50) {
+      mode = 1;
+      Second = second(); //new game debug error
     }
   }
 }
@@ -108,15 +151,42 @@ function draw() {
   if (mode == 1) {
     draw1();
   }
+
+  if (mode == 2) {
+    draw2();
+  }
 }
 
 
 function draw0() {
   background(backImgHome);
+  if (random(0, 1) < 0.025) {
+    birds.push(new Bird(random(-100, 0), random(height / 5, height * 3 / 5), 35));
+    // birds[i]=new Bird(random(0,250),random(0,575),35);
+  }
+  for (let i = 0; i < birds.length; i++) {
+    birds[i].randomMovement();
+    birds[i].show();
+    if (birds[i].body.position.x > width && birds[i].body.position.y > height) {
+      birds.splice(i, 1);
+      i--;
+    }
+
+  }
+  console.log(birds.length)
   image(playImg, 650, 375, 203, 100); //w/h ratio 2.03
+
 }
 
 function draw1() {
+  background(backImgLevel);
+  image(l1Img, 150, 300);
+  image(l2Img, 650, 300);
+  image(l3Img, 1150, 300);
+  image(backImg, 20, 50, 100, 100)
+}
+
+function draw2() {
   background(backgroundImg);
   Matter.Engine.update(engine);
   ground.show();
@@ -128,4 +198,5 @@ function draw1() {
   bird.show();
   image(slingImg2, 230, 560, 28.125, 78.125);
   slingshot.show();
+  image(backImg, 20, 50, 100, 100)
 }
